e924571beee3cab2763e031c96ea83f7711a12dc
Sabbir <67603927+sabbir-ahmed007@users.noreply.github.com>
2021-03-14 23:27:44 +0600

    Update armstrong.cpp

--- armstrong.cpp
+++ armstrong.cpp
@@ -1,16 +1,41 @@
-#include<stdio.h>
+#include <stdio.h>
+int power(int, int);
+
 int main()
 {
-    int num,remainder;
-    printf("Enter a Number: ");
-    scanf("%d",&num);
-    for(int i= 1; i<= num-1; i++)
-    {
-        remainder=num%10;
-        remainder += remainder*remainder*remainder;
-        remainder%10;
-    }
+  int n, sum = 0, t, remainder, digits = 0;
 
+  printf("Input an integer\n");
+  scanf("%d", &n);
 
+  t = n;
+  // Count number of digits
+  while (t != 0) {
+    digits++;
+    t = t/10;
+  }
 
+  t = n;
+
+  while (t != 0) {
+    remainder = t%10;
+    sum = sum + power(remainder, digits);
+    t = t/10;
+  }
+
+  if (n == sum)
+    printf("%d is an Armstrong number.\n", n);
+  else
+    printf("%d isn't an Armstrong number.\n", n);
+
+  return 0;
+}
+
+int power(int n, int r) {
+  int c, p = 1;
+
+  for (c = 1; c <= r; c++)
+    p = p*n;
+
+  return p;
 }
